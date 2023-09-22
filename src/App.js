1d910f02e56cbfbf4fde825726974e2f13e3ca1b
Prateek Kumar <singhprateek474@gmail.com>
2023-09-22 19:20:07 +0530

    NFC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState ,useEffect} from 'react';
 
 function App() {
+  const user = "";
+  const [output, setOutput] = useState("");
+  const [url, setURL] = useState(`http://qviq.io`);
+
+  useEffect(() => {
+    console.log(url);
+    
+    if ("NDEFReader" in window) {
+      setOutput("NFC writing supported in this browser.");
+    } else {
+      setOutput("NFC writing not supported in this browser.");
+    }
+  }, [url]);
+
+  const option1 = async () => {
+    console.log(url);
+    const ndef = new window.NDEFReader();
+    ndef
+      .write({
+        records: [{ recordType: "url", data: url }],
+      })
+      .then(() => {
+        console.log("Message written.");
+      })
+      .catch((error) => {
+        console.log(`Write failed :-( try again: ${error}.`);
+      });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{output}</h2>
+      <button type="button" onClick={()=>{option1()}}>Option1</button>
+      {/* <button type="button">Option2</button>
+      <button type="button">Option3</button> */}
     </div>
   );
 }
